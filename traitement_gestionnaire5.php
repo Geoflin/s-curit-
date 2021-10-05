15f6721636c7334f98f146b8aca4fc085ce40396
Guillaume Baur <guilaume.baur@gmail.com>
2021-10-05 12:12:26 +0200

    probleme_date_vide_resolu

--- traitement_gestionnaire5.php
+++ traitement_gestionnaire5.php
@@ -3,7 +3,18 @@
   <TITLE>Kinepolise/reservation/confirmation</TITLE>
  </HEAD>
 
-<?php
+ <?php
+ function unix_timestamp($datetime)
+{
+	$datetime = str_replace(array(' ', ':'), '-', $datetime);
+	$c    = explode('-', $datetime);
+	$c    = array_pad($c, 6, 0);
+	array_walk($c, 'intval');
+ 
+	return mktime($c[3], $c[4], $c[5], $c[1], $c[2], $c[0]);
+} 
+
+
     $pdo = new PDO('mysql:dbname=kinepolise;host=localhost', 'root', '');
     $kinepolise = new PDO('mysql:dbname=kinepolise;host=localhost', 'root', '');
         foreach ($pdo->query('SELECT FilmName, DateSeanceBegin, DateSeanceEnd, SalleName FROM seance_cinema1 WHERE Id=(SELECT max(Id) FROM seance_cinema1);')as $maxid) {
@@ -11,10 +22,14 @@
             $FilmName= $maxid['FilmName'];
             $DateSeanceBegin= $maxid['DateSeanceBegin'];
             $DateSeanceEnd= $maxid['DateSeanceEnd'];
+            
+            $Unix_DateSeanceBegin = unix_timestamp($maxid['DateSeanceBegin']);
+            $Unix_fusionDateBegin = unix_timestamp($fusionDateBegin_AjouterSeance);
         };
-        if ($FilmName != $_POST['FilmName']) {
-        if ($kinepolise->exec('INSERT INTO seance_cinema1 (FilmName, DateSeanceBegin, DateSeanceEnd, SalleName, Nombre_de_place) VALUES ("'. $_POST['FilmName'] . '", "' . $fusionDateBegin_AjouterSeance . '", "' . $fusionDateEnd_AjouterSeance .'", "' . $_POST['SalleName'] .'", "' . $_POST['Nombre_de_place'] .'");') !== false) {
-
-        } else { echo 'On peut pas mettre une séance en double !';
-            }
+        if (isset($Unix_DateSeanceBegin)) {
+          if (($FilmName == $_POST['FilmName'] && $Unix_DateSeanceBegin != $Unix_fusionDateBegin) || ($FilmName != $_POST['FilmName'] && $Unix_DateSeanceBegin != $Unix_fusionDateBegin)) {
+            if ($kinepolise->exec('INSERT INTO seance_cinema1 (FilmName, DateSeanceBegin, DateSeanceEnd, SalleName, Nombre_de_place) VALUES ("'. $_POST['FilmName'] . '", "' . $fusionDateBegin_AjouterSeance . '", "' . $fusionDateEnd_AjouterSeance .'", "' . $_POST['SalleName'] .'", "' . $_POST['Nombre_de_place'] .'");') !== false) {}
+                }
+            } else {
+              if ($kinepolise->exec('INSERT INTO seance_cinema1 (FilmName, DateSeanceBegin, DateSeanceEnd, SalleName, Nombre_de_place) VALUES ("'. $_POST['FilmName'] . '", "' . $fusionDateBegin_AjouterSeance . '", "' . $fusionDateEnd_AjouterSeance .'", "' . $_POST['SalleName'] .'", "' . $_POST['Nombre_de_place'] .'");') !== false) {}
         }
