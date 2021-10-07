4a16998981b2c03016aa0e114db3528899f257d5
Guillaume Baur <guilaume.baur@gmail.com>
2021-10-07 10:47:44 +0200

    calcul_fin_seance_dans_modifierSeance_ok

--- Modifier_seance1.php
+++ Modifier_seance1.php
@@ -1,31 +1,13 @@
 <?php
-                             //debug
-                             $debug = false;
-                             if ($debug) {
-                             
-                                 // gère et affiche tous les niveaux d'erreurs en mode débogage
-                             
-                                 error_reporting(E_ALL);
-                             
-                                 ini_set('display_errors', '1');
-                             
-                             } else {
-                             
-                                 // en mode production, ne gère pas certains niveaux pour des raisons de performance (ceux précédés de ~), tel que suggéré dans php.ini
-                             
-                                 // même pour les niveaux gérés, aucun message ne sera affiché
-                             
-                                 error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
-                             
-                                 ini_set('display_errors', '0');
-                             
-                             }
-                             //debug
+
+                                                                                //on calcul et ajoute date de fin séance
+                  require_once 'Calcul_fin_seance.php';
+                  require_once 'fusion_dateTime.php';
+
                              $Id= implode("", $_POST['Id']);
                                  $conn = new PDO('mysql:dbname=kinepolise;host=localhost', 'root', '');
                                  $conn->exec("SET CHARACTER SET utf8");
-                                 
-                                         $sql = "UPDATE `seance_cinema1` SET `FilmName` = '".$_POST['FilmName']."', `DateSeanceBegin` = '".$fusionDateBegin_AjouterSeance."', `DateSeanceEnd` = '".$fusionDateEnd_AjouterSeance."', `SalleName` = '".$_POST['SalleName']."' WHERE `seance_cinema1`.`Id` = '".$Id."' ";
+                                         $sql = "UPDATE `seance_cinema1` SET `FilmName` = '".$_POST['FilmName']."', `DateSeanceBegin` = '".$fusionDateBegin_AjouterSeance."', `SalleName` = '".$_POST['SalleName']."' WHERE `seance_cinema1`.`Id` = '".$Id."' ";
                                          $count = $conn->exec($sql);
                                  
                                          $conn = null;
