95c5723a223534d144a837e958ea9cb03267f725
Guillaume Baur <guilaume.baur@gmail.com>
2021-10-18 19:22:48 +0200

    tri_client_checkpoint

--- Admin_principal/exploration_reservation_client.php
+++ Admin_principal/exploration_reservation_client.php
@@ -13,8 +13,7 @@
     body {
         font-family: Calibri, serif;
         display: grid;
-        grid-template-columns: 10% 90%;
-        grid-template-rows:50px 100px 100px 70px 1fr;
+        grid-template-rows:50px 1fr 1fr 1fr 1fr 1fr;
         background-color: black;
         color: white;
     }
@@ -31,11 +30,8 @@
       background-color:rgb(69, 69, 69);
         font-weight: bold;
     }
-    .ligne4, .table2, .ligne6, .table4, .ligne8, .table6{
-        border-collapse: collapse;
-        width: 100%;
-        height: 300px;
-        background-color: rgb(39, 39, 39);
+    table{
+        background-color: rgba(39, 39, 39);
     }
     nav{
         grid-column: 1/3;
@@ -50,86 +46,27 @@
       height: 25px;
       margin-left: 10px;
     }
-    .center{
-      display: flex;
-      justify-content: center;
-      background-color: rgb(39, 39, 39);
-      z-index: 1;
-    }
-    .ligne1{
-        grid-column: 1/3;
-        grid-row: 2/2;
-        display: flex;
-        justify-content: center;
-    }
     .ligne2{
-        grid-column: 1/3;
-        grid-row: 3/3;
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-        margin-left:300px;
-        margin-right:300px;
+        grid-row: 2/2;
     }
     .ligne3{
-      grid-row: 4/4;
-      display: flex;
-      justify-content: flex-start;
-      align-items: flex-end;
-      margin-bottom: 0px;
+        grid-row: 3/3;
     }
-    .title1{
-      grid-column: 1/3;
-      grid-row: 4/4;
-      text-align: center;
-      text-decoration: underline;
-      color:rgb(155, 89, 182);
-      display: flex;
-      align-items:flex-end;
-      justify-content: ;
-      padding: 0px;
-      margin: 0px;
-    }
-    /*tableau1*/
     .ligne4{
-      grid-row: 5/5;
-      grid-column: 2/2;
+        grid-row: 4/4;
     }
-    .table2{
-      grid-row: 5/5;
-      grid-column: 1/1;
+    .ligne5{
+        grid-row: 5/5;
     }
-      .ligne5{
-      grid-row: 6/6;
-      display: flex;
-      justify-content: ;
-      align-items: flex-end;
-      margin-bottom:0px;
+    .ligne3, .table2, .ligne6, .table4, .ligne8, .table6{
+        border-collapse: collapse;
+        width: 100%;
+        height: 300px;
+        background-color: rgb(39, 39, 39);
     }
-    /*tableau2*/
-      .ligne6{
-      grid-row: 7/7;
-      grid-column: 2/2;
-    }
-    .table4{
-      grid-row: 7/7;
-      grid-column: 1/1;
-    }
-    .ligne7{
-      grid-row: 8/8;
-      display: flex;
-      justify-content: ;
-      align-items: flex-end;
-      margin-bottom:0px;
-    }
-    /*tableau3*/
-    .ligne8{
-      grid-row: 9/9;
-      grid-column: 2/2;
-    }
-    .table6{
-      grid-row: 9/9;
-      grid-column: 1/1;
+    .center{
+        display: flex;
+        justify-content: center;
     }
     .type_of_tri{
       display: flex;
@@ -138,6 +75,12 @@
       flex-direction: row;
     }
     </style>
+
+      <!--Function Tri-->
+  <h2 class="ligne1">Tri de l'affichage</h2>
+  <span class="ligne2">
+  <?php require_once 'exploration_reservation_client.php'; ?>
+  </span>
 
 <nav>
 <button name="accueil"><a href="../../../index.php">retour à l'accueil</a></button>
@@ -160,14 +103,14 @@ session_start();
 if ((($_SESSION['username'] == $dataConnexion['username']  && $_SESSION['password'] == $dataConnexion['password']) || ($_SESSION['username'] == $adminConnexion['username']  && $_SESSION['password'] == $adminConnexion['password']) )) {
   echo sprintf("<nav class=center><h3>Vous êtes connecté, bonjour %s <h3/></nav>", $_SESSION['username']) . PHP_EOL; 
 ?>
-    <!--Actualiser la page-->
-    <h2 class="ligne3">Liste des réservations</h2>
-    <!--Tableaux-->
+
+<!--Tableaux-->
+    <h2 class="ligne2 center">Liste des réservations</h2>
     <!-- Thead-->
     <?php
         $pdo_kinepolise_client = new PDO('mysql:host=localhost;dbname=kinepolise_client', 'root', '');?>
 
-        <table class="ligne4">
+        <table class="ligne3">
         <tr class="thead">
             <!-- Form  triNomFilm-->
         <td>Nom d'utilisateur</td>
@@ -205,6 +148,49 @@ if ((($_SESSION['username'] == $dataConnexion['username']  && $_SESSION['passwor
         <?php }; ?>
     </table>
 
+    <!--Tableaux-->
+    <h2 class="ligne4 center">Liste des réservations</h2>
+    <!-- Thead-->
+    <?php
+        $pdo_kinepolise_client = new PDO('mysql:host=localhost;dbname=kinepolise_client', 'root', '');?>
+
+        <table class="ligne5">
+        <tr class="thead">
+            <!-- Form  triNomFilm-->
+        <td>Nom d'utilisateur</td>
+        <td>Mot de passe</td>
+        <td>Nom du film</td>
+        <td>Heure de début</td>
+        <td>Heure de fin</td>
+        <td>Salle</td>
+</tr>
+    </form>
+
+        <!-- Boucle Corps du tableau-->
+        <?php
+        $pdo_kinepolise_client= new PDO('mysql:host=localhost;dbname=kinepolise_client', 'root', '');
+    foreach ($pdo_kinepolise_client->query('SELECT * FROM reservation_client', PDO::FETCH_ASSOC) as $seance) { 
+      $dateSeanceBegin = new DateTime($seance['DateSeanceBegin']);
+      $DateSeanceEnd = new DateTime($seance['DateSeanceEnd']);
+            ?>
+        <!-- Form  modifierseance-->
+        <form class="modifierSeance" method="post" action="">
+
+        <tr class=<?php echo $seance['FilmName']?>>
+
+        <td><?php echo $seance['username'];?></td>
+        <td><?php echo $seance['FilmName'];?></td> 
+        <td id="Colonne3"><?php echo $dateSeanceBegin->format('Y-m-d');?><br/></td>
+        <td id="Colonne4"><?php echo $dateSeanceBegin->format('H:i');?><br/></td>
+        <td id="Colonne5"><?php echo $DateSeanceEnd->format('H:i');?></td>
+        <td id="Colonne6"><?php echo $seance['SalleName'];?></br></td>
+
+        </tr>
+
+        </form>
+        
+        <?php }; ?>
+    </table>
 
     </body>
 
---
+++ Admin_principal/tri_reservation.php
@@ -0,0 +1,34 @@
+       <!-- Tri_par_jour_de_seance-->
+       <span class="blockTri2">
+
+  <form method="POST" action="">
+  <div class="type_of_tri">Tri par jour de seance</div></br>
+  <select name="dateSeanceBeginTest">
+  <option id="dateSeanceBegin" value=" ">Tout afficher<br></option>
+  <?php 
+  $pdo_kinepolise_cinema1 = new PDO('mysql:host=localhost;dbname=kinepolise_cinema1', 'root', '');
+  foreach ($pdo_kinepolise_cinema1->query('SELECT dateSeanceBegin FROM seance_cinema1', PDO::FETCH_ASSOC) as $date) { ?>
+                    <option><?php echo $date['dateSeanceBegin'].'<br>'; ?></option>
+  <?php } ?>
+  </select>
+  <input type="SUBMIT" value="Tri !" name="triSeanceBegin">
+  </form>
+  </span>
+
+  <?php if(isset($_POST['triSeanceBegin'])){?>
+    <?php
+  $pdo_kinepolise_cinema1 = new PDO('mysql:host=localhost;dbname=kinepolise_cinema1', 'root', '');
+ foreach ($pdo_kinepolise_cinema1->query('SELECT * FROM seance_cinema1 WHERE dateSeanceBegin= "'.$_POST['dateSeanceBeginTest'].'" ', PDO::FETCH_ASSOC) as $FilmSalle) {
+  if(isset($FilmSalle['FilmName'])){
+    $FilmSalle1= $FilmSalle['FilmName'];
+    $SalleName= $FilmSalle['FilmName'];
+  } else {
+    $FilmSalle1= "null";
+  };
+  } ?>
+      <style>
+        tr:not(.<?php echo $FilmSalle1 ?>, .thead  .<?php echo $SalleName ?>){
+        display: none;
+      }
+    </style>
+    <?php }; ?>
