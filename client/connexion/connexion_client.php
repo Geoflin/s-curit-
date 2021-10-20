369cc1f67c524e0e1896433345b103bc149e929d
Guillaume Baur <guilaume.baur@gmail.com>
2021-10-20 17:02:48 +0200

    externalisation_des_css_ok

--- client/connexion/connexion_client.php
+++ client/connexion/connexion_client.php
@@ -12,9 +12,6 @@
 <!--On permet au client de ce connecter-->
 <h2 class="center">Je veux me connecter à mon compte</h2> 
 
-
-
-
 <!--On permet au client de ce connecter-->
 <form method="post" action="">
     <label for="username">utilisateur</label>
@@ -49,77 +46,7 @@
     <button name="creation_client" type="submit">connexion</button>
     </form>
 
-    <style>
-    a, h2{
-      color:rgb(155, 89, 182);
-    }
-    body {
-        font-family: Calibri, serif;
-        background-color: black;
-        color: white;
-        display: grid;
-        grid-template-rows:1fr 1fr 1fr;
-        row-gap: 10px;
-    }
-    nav{
-        grid-row: 1/1;
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-    }
-    nav button{
-      background-color: white;
-      z-index: 2;
-      height: 25px;
-      margin-left: 10px;
-    }
-    .center{
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background-color: rgb(39, 39, 39);
-      z-index: 1;
-      height: 50px;
-      margin-left: 10%;
-      margin-right: 10%;
-    }
-    form {
-        max-width: 50%;
-        margin-left: 500px;
-    }
-    form label {
-        display: block;
-        font-weight: bold;
-        margin-bottom: 10px;
-    }
-    label.inline-label {
-        display: inline-block;
-    }
-    fieldset {
-        border: 1px solid lightgray;
-        background-color: rgba(225, 233, 255, 0.25);
-    }
-    legend {
-        font-style: italic;
-        font-size: 1.1em;
-        padding: 5px;
-    }
-    form input, form select, form textarea {
-        display: inline-block;
-        margin-bottom: 10px;
-        padding: 10px;
-        width: 80%;
-    }
-    form input[type="radio"],
-    form input[type="checkbox"],
-    form input[type="submit"] {
-        width: auto;
-    }
-    button[type=submit], button[type=reset] {
-        padding: 10px;
-        margin-top: 15px;
-    }
-</style> 
+    <link rel="stylesheet" href="connexion_client.css" />
 
     <!--On traite la connexion au compte-->
     <?php
