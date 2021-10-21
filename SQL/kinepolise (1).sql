245b6f9cefddbcfe11f8327dfa16e9f697dcafa6
Guillaume Baur <guilaume.baur@gmail.com>
2021-10-21 10:21:35 +0200

    creation_sql_kinepolise_administrateur

--- SQL/kinepolise (1).sql
+++ SQL/kinepolise (1).sql
@@ -1,11 +1,9 @@
-DROP SCHEMA IF EXISTS kinepolise2;
-CREATE SCHEMA kinepolise2;
+--
+-- Base de données : `stream`
+DROP SCHEMA IF EXISTS stream;
+CREATE SCHEMA stream;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-CREATE TABLE kinepolise2.info_film (
+CREATE TABLE stream.info_film (
   `Id` int(11) NOT NULL,
   `FilmName` varchar(250) NOT NULL,
   `Duree` time NOT NULL
@@ -15,7 +13,7 @@ CREATE TABLE kinepolise2.info_film (
 -- Déchargement des données de la table `info_film`
 --
 
-INSERT INTO kinepolise2.info_film (`Id`, `FilmName`, `Duree`) VALUES
+INSERT INTO stream.info_film (`Id`, `FilmName`, `Duree`) VALUES
 (2, 'Tron', '02:00:00'),
 (3, 'Grand_Torino', '01:30:00'),
 (15, 'Oblivion', '02:00:00'),
@@ -27,7 +25,7 @@ INSERT INTO kinepolise2.info_film (`Id`, `FilmName`, `Duree`) VALUES
 -- Structure de la table `password`
 --
 
-CREATE TABLE kinepolise2.password (
+CREATE TABLE stream.password (
   `Id` int(11) NOT NULL,
   `username` varchar(250) NOT NULL,
   `password` varchar(250) NOT NULL
@@ -37,7 +35,7 @@ CREATE TABLE kinepolise2.password (
 -- Déchargement des données de la table `password`
 --
 
-INSERT INTO kinepolise2.password (`Id`, `username`, `password`) VALUES
+INSERT INTO stream.password (`Id`, `username`, `password`) VALUES
 (1, 'john', 'ripples1947'),
 (3, 'Billy', 'billy1');
 
@@ -48,13 +46,13 @@ INSERT INTO kinepolise2.password (`Id`, `username`, `password`) VALUES
 --
 -- Index pour la table `info_film`
 --
-ALTER TABLE kinepolise2.info_film
+ALTER TABLE stream.info_film
   ADD PRIMARY KEY (`Id`);
 
 --
 -- Index pour la table `password`
 --
-ALTER TABLE kinepolise2.password
+ALTER TABLE stream.password
   ADD PRIMARY KEY (`Id`);
 
 --
@@ -64,12 +62,12 @@ ALTER TABLE kinepolise2.password
 --
 -- AUTO_INCREMENT pour la table `info_film`
 --
-ALTER TABLE kinepolise2.info_film
+ALTER TABLE stream.info_film
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT pour la table `password`
 --
-ALTER TABLE kinepolise2.password
+ALTER TABLE stream.password
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
--- SQL/kinepolise_administrateur (1).sql
+++ SQL/kinepolise_administrateur (1).sql
@@ -1,33 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : jeu. 21 oct. 2021 à 09:35
--- Version du serveur : 10.4.21-MariaDB
--- Version de PHP : 8.0.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
+-- Base de données : `stream_administrateur2`
+DROP SCHEMA IF EXISTS stream_administrateur2;
+CREATE SCHEMA stream_administrateur2;
 --
--- Base de données : `kinepolise_administrateur`
---
-
--- --------------------------------------------------------
-
 --
 -- Structure de la table `password`
 --
 
-CREATE TABLE `password` (
+CREATE TABLE stream_administrateur2.password (
   `Id` varchar(11) NOT NULL,
   `username` varchar(110) NOT NULL,
   `password` varchar(110) NOT NULL
@@ -37,7 +17,7 @@ CREATE TABLE `password` (
 -- Déchargement des données de la table `password`
 --
 
-INSERT INTO `password` (`Id`, `username`, `password`) VALUES
+INSERT INTO stream_administrateur2.password  (`Id`, `username`, `password`) VALUES
 ('1', 'admin', 'inputBox');
 
 --
@@ -47,10 +27,7 @@ INSERT INTO `password` (`Id`, `username`, `password`) VALUES
 --
 -- Index pour la table `password`
 --
-ALTER TABLE `password`
+ALTER TABLE stream_administrateur2.password 
   ADD PRIMARY KEY (`Id`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
