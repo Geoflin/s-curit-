9656f7e47fe85d460373ec0b72037e33d5ed738e
Guillaume Baur <guilaume.baur@gmail.com>
2021-10-24 09:58:34 +0200

    new heroku cleardb configuration

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit485aa9fe8733cbcc9234357aae0730e7
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit485aa9fe8733cbcc9234357aae0730e7', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit485aa9fe8733cbcc9234357aae0730e7', 'loadClassLoader'));
@@ -52,24 +50,6 @@ class ComposerAutoloaderInit485aa9fe8733cbcc9234357aae0730e7
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInit485aa9fe8733cbcc9234357aae0730e7::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire485aa9fe8733cbcc9234357aae0730e7($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequire485aa9fe8733cbcc9234357aae0730e7($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
